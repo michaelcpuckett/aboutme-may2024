447c9feda2d138d3bf89de747e5f4b3a9a64d41e
cjmet <khaaiev@gmail.com>
2024-08-26 19:46:27 -0400

    Prototype to switch pages(content), but remain in a single page

--- public/chris_metcalfe/script.js
+++ public/chris_metcalfe/script.js
@@ -3,9 +3,11 @@ DemoMessage('PS: JavaCat says "Hello World!"');
 document.getElementById("PowerButton").addEventListener("click", function () {
   // console.log('JavaCat writes "Goodbye World!"');
   // DemoMessage('PS: JavaCat says "Goodbye World!"');
-  if (document.getElementById('Main').style.display != 'none') {
-    document.getElementById('Main').style.display = 'none';
+  if (document.getElementById("About").style.display != "none") {
+    document.getElementById("About").style.display = "none";
+    document.getElementById("Game").style.display = "flex";
   } else {
-    document.getElementById('Main').style.display = 'flex';
+    document.getElementById("About").style.display = "flex";
+    document.getElementById("Game").style.display = "none";
   }
 });
