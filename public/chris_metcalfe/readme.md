c43b6aa055eba363c9099c87756d81cab292ba37
cjmet <khaaiev@gmail.com>
2024-08-27 15:22:10 -0400

    Worked on Nav Buttons and other changes

---
+++ public/chris_metcalfe/readme.md
@@ -0,0 +1,10 @@
+#Blog
+
+## 24/08/27
+
+- A little late to the show with the blog and notes....
+- Figured out the layout to have multiple vertical pages of equal height and commented it in the code.
+- Set the page back to a single page that swaps the other pages in and out.
+- Initial prototype of the two page design
+- `displayValue = window.getComputedStyle(element).display;` unless you force set the value first. Otherwise it often reads '' on the first pass.
+- Worked on nav buttons, and added WeatherKitty placeholder.
--- public/chris_metcalfe/script.js
+++ public/chris_metcalfe/script.js
@@ -1,13 +1,39 @@
+// addEventListener("load", ... );
+
 console.log('JavaCat writes "Hello World!"');
 DemoMessage('PS: JavaCat says "Hello World!"');
+
 document.getElementById("PowerButton").addEventListener("click", function () {
-  // console.log('JavaCat writes "Goodbye World!"');
-  // DemoMessage('PS: JavaCat says "Goodbye World!"');
-  if (document.getElementById("About").style.display != "none") {
-    document.getElementById("About").style.display = "none";
-    document.getElementById("Game").style.display = "flex";
-  } else {
-    document.getElementById("About").style.display = "flex";
-    document.getElementById("Game").style.display = "none";
-  }
+  console.log("PowerButton Clicked");
+  ToggleDisplayFlex("Main");
 });
+
+document.getElementById("AboutMeButton").addEventListener("click", function () {
+  console.log("About Me Clicked");
+  document.getElementById("Main").style.display = "flex";
+  document.getElementById("AboutMe").style.display = "flex";
+  document.getElementById("Game").style.display = "none";
+});
+
+document.getElementById("GameButton").addEventListener("click", function () {
+  console.log("Game Clicked");
+  document.getElementById("Main").style.display = "flex";
+  document.getElementById("AboutMe").style.display = "none";
+  document.getElementById("Game").style.display = "flex";
+});
+
+// Functions ...
+
+function ToggleDisplayFlex(elementIdName) {
+  let element = document.getElementById(elementIdName);
+  let displayValue = element.style.display;
+  if (displayValue === "")
+    displayValue = window.getComputedStyle(element).display;
+
+  // console.log(`ToggleDisplayFlex: ${elementIdName} =/= ${displayValue}`);
+  if (displayValue === "flex") {
+    element.style.display = "none";
+  } else {
+    element.style.display = "flex";
+  }
+}
